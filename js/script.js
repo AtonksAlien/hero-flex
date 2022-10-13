3c54f94f052df12635cbbf6fc92b3678ba4a2a9d
AtonksAlien <losaccoandrea04@gmail.com>
2022-10-13 11:49:06 +0200

    Menu completed
    finally all animations are fixed.
    Full focus on next feature 💪🤟

--- js/script.js
+++ js/script.js
@@ -7,21 +7,36 @@ cross = document.querySelector('.cross')
 menutbtn.addEventListener('click', function() {
     if (items.classList.contains('hidden')) {
         items.classList.remove('hidden')
-/*      cross.classList.remove('hidden')
-*/      items.classList.add('slide-in-left')
+        items.classList.add('slide-in-left')
         items.classList.remove('slide-out-left')
 
-/*      ham.classList.toggle('fade-out')
-        cross.classList.toggle('fade-in')
- */    } else {
-        items.classList.remove('slide-in-left')
+        ham.classList.add('fade-out')
+        setTimeout(function() {
+            ham.classList.add('hidden')
+            ham.classList.remove('fade-out')
+            cross.classList.remove('hidden')
+            cross.classList.add('fade-in')
+        }, 250)
+        setTimeout(function() {
+            cross.classList.remove('fade-in')
+        }, 500)
 
+    } else {
+        items.classList.remove('slide-in-left')
         items.classList.add('slide-out-left')
         setTimeout(function() {
             items.classList.add('hidden')
         }, 500)
-/*      ham.classList.toggle('fade-in')
-        cross.classList.toggle('fade-out')
- */
+
+        cross.classList.add('fade-out')
+        setTimeout(function() {
+            cross.classList.add('hidden')
+            cross.classList.remove('fade-out')
+            ham.classList.remove('hidden')
+            ham.classList.add('fade-in')
+        }, 250)
+        setTimeout(function() {
+            ham.classList.remove('fade-in')
+        }, 500)
     }    
 })
