012951e4ab2f6bd4894d0f3195859690c80443c8
AtonksAlien <losaccoandrea04@gmail.com>
2022-10-13 09:19:26 +0200

    fixed slide menu bar
    finally fixed slide menu bar

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 menutbtn = document.querySelector('.hamburger')
 items = document.querySelector('.menu-items')
+links = document.querySelectorAll('li a')
 ham = document.querySelector('.ham')
 cross = document.querySelector('.cross')
-menutbtn.addEventListener('click', function() {
-    //cross.classList.toggle('flip-vertical-right')
 
+menutbtn.addEventListener('click', function() {
     if (items.classList.contains('hidden')) {
         items.classList.remove('hidden')
-        cross.classList.remove('hidden')
-        items.classList.toggle('slide-in-left')
-        ham.classList.toggle('fade-out')
-        cross.classList.toggle('fade-in')
-    } else {
-        items.classList.toggle('slide-out-left')
-        ham.classList.toggle('fade-in')
-        cross.classList.toggle('fade-out')
+/*      cross.classList.remove('hidden')
+*/      items.classList.add('slide-in-left')
+        items.classList.remove('slide-out-left')
 
+/*      ham.classList.toggle('fade-out')
+        cross.classList.toggle('fade-in')
+ */    } else {
+        items.classList.remove('slide-in-left')
+
+        items.classList.add('slide-out-left')
+        setTimeout(function() {
+            items.classList.add('hidden')
+        }, 500)
+/*      ham.classList.toggle('fade-in')
+        cross.classList.toggle('fade-out')
+ */
     }    
 })
